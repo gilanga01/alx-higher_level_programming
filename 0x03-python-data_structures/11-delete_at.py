4bd065a26f8f980f06c6e3a7d4944241ecc26f8b
Langat <langatgilbert2013@gmail.com>
2023-12-07 23:28:57 -0800

    correction

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to delete the item at a specific position in a list
-#
-# 
-# -----------------------------------------------------------
-
-
 def delete_at(my_list=[], idx=0):
-    """deletes the item at a specific position in a list
-
-    Args:
-        my_list: a list
-        idx: an integer representing an index position
-
-    Returns:
-        list without deleted item
-    """
-
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    my_list.remove(my_list[idx])
-    return my_list
+    length_list = len(my_list) - 1
+    if idx < 0 or idx > length_list:
+        return(my_list)
+    else:
+        del my_list[idx]
+        return(my_list)
